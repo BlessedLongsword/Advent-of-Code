66f51f81a06de1b5d6402dfcbf0cdd98084bbf13
BlessedLongsword <atigriva41@alumnes.ub.edu>
2024-02-27 17:53:54 +0100

    day 9

--- 2015/Day 09/day9.py
+++ 2015/Day 09/day9.py
@@ -25,7 +25,9 @@ Belfast -> London -> Dublin = 982
 The shortest of these is London -> Dublin -> Belfast = 605, and so the answer is 605 in this example.
 
 What is the distance of the shortest route?
-"""             
+"""
+
+from itertools import permutations    
         
 def get_cities(file):
     cities = {}
@@ -42,4 +44,37 @@ def get_cities(file):
             cities[dest] = {origin: int(distance)} 
     return cities
 
-print(get_cities("input.txt"))
+
+def shortest_route(cities):
+    min_dist = float('inf')
+    for route in permutations(cities):
+        traveled_dist = 0
+        for i in range(len(route) - 1):
+            traveled_dist += cities[route[i]][route[i + 1]]
+        min_dist = min(min_dist, traveled_dist)
+    return min_dist
+            
+                
+print("The distance of the shortest route is", shortest_route(get_cities("input.txt")))
+
+"""
+--- Part Two ---
+The next year, just to show off, Santa decides to take the route with the longest distance instead.
+
+He can still start and end at any two (different) locations he wants, and he still must visit each location exactly once.
+
+For example, given the distances above, the longest route would be 982 via (for example) Dublin -> London -> Belfast.
+
+What is the distance of the longest route?
+"""
+
+def longest_route(cities):
+    max_dist = 0
+    for route in permutations(cities):
+        traveled_dist = 0
+        for i in range(len(route) - 1):
+            traveled_dist += cities[route[i]][route[i + 1]]
+        max_dist = max(max_dist, traveled_dist)
+    return max_dist
+
+print("The distance of the longest route is", longest_route(get_cities("input.txt")))
