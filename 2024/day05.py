75a191ee5ef18dd8666f0340d199749d085f9538
Antonio Tigri <antonio.tigri@proton.me>
2024-12-05 22:59:45 +0100

    Day 5

---
+++ 2024/day05.py
@@ -0,0 +1,51 @@
+# Day 5
+
+# Input
+
+ordering_rules = set()
+updates = list()
+with open('./inputs/day05.txt') as f:
+    first_section = True
+    for line in f.read().splitlines():
+        if first_section:
+            if not line:
+                first_section = False
+                continue
+            ordering_rules.add(tuple(map(int, line.split('|'))))
+        else:
+            updates.append(list(map(int, line.split(','))))
+
+
+from functools import cmp_to_key
+
+correctly_ordered_updates = list()
+
+def compare_update_numbers(number1, number2):
+    if (number1, number2) in ordering_rules:
+        return -1
+    elif (number2, number1) in ordering_rules:
+        return 1
+    else:
+        return 0
+
+correctly_ordered_updates = list() # Part 1
+incorrectly_ordered_updates_sorted = list() # Part 2
+
+for update in updates:
+    sorted_update = sorted(update, key=cmp_to_key(compare_update_numbers))
+    if update == sorted_update: 
+        correctly_ordered_updates.append(sorted_update)
+    else:
+        incorrectly_ordered_updates_sorted.append(sorted_update)
+
+def get_middle_number_sum(updates):
+    return sum(update[len(update) // 2] for update in updates)
+
+# Part 1
+
+print(f'If we add up the middle page number of the correctly-ordered updates we get {get_middle_number_sum(correctly_ordered_updates)}.')
+
+
+# Part 2
+
+print(f'If we add up the middle page number of the incorrectly-ordered updates after sorting them we get {get_middle_number_sum(incorrectly_ordered_updates_sorted)}.')
